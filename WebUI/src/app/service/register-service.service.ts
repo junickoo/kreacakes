6408a91f3df228c5a133d07a06997acd1766b721
Rayhan Junicko <rjunicko@gmail.com>
2022-11-16 16:27:55 +0700

    Update register-service.service.ts

--- WebUI/src/app/service/register-service.service.ts
+++ WebUI/src/app/service/register-service.service.ts
@@ -1,11 +1,18 @@
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DialogOverviewComponent } from '../layout/dialog-overview/dialog-overview.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterServiceService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
   registerAcc(
     inUsername: any,
     inPassword: any,
@@ -26,11 +33,31 @@ export class RegisterServiceService {
 
   setRegis(data: any) {
     if (data.status == '200') {
-      sessionStorage.setItem('isRegister', 'true');
-      alert(data.message);
-      window.location.reload();
+      const dialogRef = this.dialog.open(DialogOverviewComponent, {
+        width: '250px',
+        data: {
+          type: 'login',
+          message: 'Register Success!',
+        },
+      });
+
+      dialogRef.afterClosed().subscribe((result: any) => {
+        this.router.navigateByUrl('/login');
+      });
     } else {
-      sessionStorage.setItem('isRegister', 'false');
+      const dialogRef = this.dialog.open(DialogOverviewComponent, {
+        width: '250px',
+        data: {
+          type: 'login',
+          message: 'Register Failed! (' + data.message + ')',
+        },
+      });
+
+      dialogRef.afterClosed().subscribe((result: any) => {
+        window.location.reload();
+      });
+
+      console.log(data);
     }
   }
 }
